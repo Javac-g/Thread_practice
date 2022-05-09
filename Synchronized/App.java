b7d3ce4cfaaf9444af08362611db08cb1efae4bb
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-09 20:32:56 +0300

    ten

--- Synchronized/App.java
+++ Synchronized/App.java
@@ -5,7 +5,11 @@ public class App {
     public static void main(String...args){
 
 
+        Call ob = new Call();
 
+        Caller A = new Caller(ob,"Welcome");
+        Caller B = new Caller(ob,"to synchronized");
+        Caller C = new Caller(ob,"world");
 
 
 
