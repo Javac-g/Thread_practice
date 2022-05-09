c52d453f40e6c453e1e8e62489d9cd5c80412224
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-05-09 17:00:41 +0300

    two

---
+++ One.java
@@ -0,0 +1,34 @@
+public class One implements Runnable{
+
+    Thread t;
+    String name;
+
+    One(String name){
+
+        this.name = name;
+        t = new Thread(this,name);
+        System.out.println("Created: " + name);
+        t.start();
+
+    }
+
+    public void run(){
+
+        try{
+            for (int i = 9; i >=0 ; i--){
+
+                System.out.println(name + ": " + i);
+
+                Thread.sleep(1000);
+
+            }
+        }catch (InterruptedException e ){
+
+            e.printStackTrace();
+
+        }
+
+
+    }
+
+}
