90868cbd3fab3deedfefa1067010b272817737fd
Maxym-Denysov <adamsmatthew70@yahoo.com>
2022-11-06 17:44:56 +0200

    Caller thread class

---
+++ V2/synchronize/Caller.java
@@ -0,0 +1,18 @@
+package V2.synchronize;
+
+public class Caller implements Runnable {
+    Thread t;
+    String msg;
+    Call call;
+    public Caller(Call call,String msg){
+        t = new Thread(this);
+        this.call = call;
+        this.msg = msg;
+        t.start();
+    }
+
+    @Override
+    public void run() {
+        call.calling(msg);
+    }
+}
